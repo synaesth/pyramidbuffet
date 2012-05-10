a7afe36e2e98a62b702a8eea5c5139777c087132
Jake Johnson <oyouareatubeo@gmail.com>
2012-05-10 16:58:58 -0700

    added mediatype examples

--- pyramidbuffet/views/details.py
+++ pyramidbuffet/views/details.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, render_template
+from jinja2 import Environment, FileSystemLoader
+
 import urllib2
 import simplejson as json
 
 mod = Blueprint('details', __name__, url_prefix='/details')
+env = Environment()
 
 
 #______________________________________________________________________________
 @mod.route("/<identifier>")
+
 def details(identifier):
     item_jstor = jstor(identifier)
     meta_dict = item_jstor['metadata']
@@ -17,3 +21,16 @@ def details(identifier):
 def jstor(identifier):
     url = 'http://archive.org/metadata/%s' % identifier
     return json.loads(urllib2.urlopen(url).read())
+
+def is_movie(mediatype):
+    if mediatype == 'movies': return True
+
+def is_audio(mediatype):
+    if mediatype == 'audio': return True
+
+def is_texts(mediatype):
+    if mediatype == 'texts': return True
+
+env.tests['movies'] = is_movie
+env.tests['audio'] = is_audio
+env.tests['texts'] = is_texts
